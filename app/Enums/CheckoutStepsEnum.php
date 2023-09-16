8400cbbf856684e0737c414d6fd9954e3e3e7488
Angelica Resende <angelica.resende@ufenesp.com.br>
2023-09-16 19:28:23 -0300

    dia 2 e 3

--- app/Enums/CheckoutStepsEnum.php
+++ app/Enums/CheckoutStepsEnum.php
@@ -4,18 +4,18 @@ namespace App\Enums;
 
 enum CheckoutStepsEnum: int {
 
-  case INFORMATION = 1;
-  case SHIPPING = 2;
-  case PAYMENT = 3;
+    case INFORMATION = 1;
+    case SHIPPING = 2;
+    case PAYMENT = 3;
 
-  public function getName(): string
-  {
-      return match ($this)
-      {
-          self::INFORMATION => 'Informações',
-          self::SHIPPING => 'Frete',
-          self::PAYMENT => 'Pagamento',
-          default => 'Passo do checkout não encontrado'
-      };
-  }
+    public function getName(): string
+    {
+        return match ($this)
+        {
+            self::INFORMATION => 'Informações',
+            self::SHIPPING => 'Frete',
+            self::PAYMENT => 'Pagamento',
+            default => 'Passo do checkout não encontrado'
+        };
+    }
 }
